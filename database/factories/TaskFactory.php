aebc80c3832071ab4e8fc8c208f65c85fa308f75
khaled.sebti <khaled.sebti@beyn.io>
2023-11-25 10:56:30 +0100

    Implement data model for task entity

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $priorities = ['low', 'medium', 'high'];
+
+        return [
+            'title' => $this->faker->sentence(5, true),
+            'priority' => $priorities[array_rand($priorities)],
+        ];
+    }
+}
